4d941bd49806b1ae6e3d0b10a07c7fd6b135190b
rohitkmrr <rkumar17094@gmail.com>
2025-10-31 23:40:37 +0530

    Update org618-user-flow-1.js

--- test-plans/playwright/org618-user-flow-1.js
+++ test-plans/playwright/org618-user-flow-1.js
@@ -25,34 +25,34 @@ test('Salesforce Recording - Wed Oct 09 2024 12:49:56 GMT+0530 (India Standard T
 
   await page.click('div > div input[type="submit"]')
 
-  await page.waitForURL('https://synthetic618org28a.ist28.lightning.stm.force.com/lightning/page/home', {
-    waitUntil: 'networkidle',
-  })
+  // await page.waitForURL('https://synthetic618org28a.ist28.lightning.stm.force.com/lightning/page/home', {
+  //   waitUntil: 'networkidle',
+  // })
 
-    await page.click(
-      'div[aria-label="App"] button[title="App Launcher"] div.slds-r5',
-    );
-  await page.fill(
-    'lightning-input.searchBar > lightning-primitive-input-simple input[type="search"]',
-    'lightning',
-  );
+  //   await page.click(
+  //     'div[aria-label="App"] button[title="App Launcher"] div.slds-r5',
+  //   );
+  // await page.fill(
+  //   'lightning-input.searchBar > lightning-primitive-input-simple input[type="search"]',
+  //   'lightning',
+  // );
 
-  await page.waitForURL('https://synthetic618org28a.ist28.lightning.stm.force.com/lightning/page/home', {
-    waitUntil: 'networkidle',
-  })
+  // await page.waitForURL('https://synthetic618org28a.ist28.lightning.stm.force.com/lightning/page/home', {
+  //   waitUntil: 'networkidle',
+  // })
 
-  await page.click('a[data-label="Lightning"] > div.slds-size_small b')
+  // await page.click('a[data-label="Lightning"] > div.slds-size_small b')
 
-  await delay(5000)
+  // await delay(5000)
   
-  await page.click(
-    'button[type="button"] > div.tooltipTrigger span.uiImage',
-  );
+  // await page.click(
+  //   'button[type="button"] > div.tooltipTrigger span.uiImage',
+  // );
   
-  page.click('div.panel-content > div.oneUserProfileCard a.logout')
-  await delay(5000)
+  // page.click('div.panel-content > div.oneUserProfileCard a.logout')
+  // await delay(5000)
 
-  page.waitForURL('https://synthetic618org28a.ist28.my.stm.salesforce.com/', {
-    waitUntil: 'networkidle',
-  })
+  // page.waitForURL('https://synthetic618org28a.ist28.my.stm.salesforce.com/', {
+  //   waitUntil: 'networkidle',
+  // })
 });
